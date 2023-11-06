49798cf38a8f87f790c8e0111739fd47676f0638
Jonas Wettergrund <jonas.wettergrund@gmail.com>
2023-11-06 20:18:27 +0100

    Add project files.

---
+++ OpenAi/MessageModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenAi
+{
+    internal class MessageModel
+    {
+        public enum Role
+        {
+            System, 
+            User    
+        }
+
+        private Role UserRole { get; set; }
+        public string Message { get; set; }
+
+        // Property to get the role in the format expected by the API
+        public string role => UserRole.ToString().ToLowerInvariant();
+
+        // Constructor to set the role and message
+        public MessageModel(Role userRole, string message)
+        {
+            UserRole = userRole;
+            Message = message;
+        }
+
+        // Method to create the content field for serialization
+        public object ToRequestMessage()
+        {
+            return new { role = this.role, content = this.Message };
+        }
+
+    }
+}
---
+++ OpenAi/OpenAiApiFetcher.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+
+namespace OpenAi
+{
+    public class OpenAiApiFetcher
+    {
+        private readonly HttpClient _httpClient;
+        private readonly MessageModel _system;
+        private readonly MessageModel _user;
+
+
+        public OpenAiApiFetcher(string apiKey, string instruction,  string input)
+        {
+
+            _system = new MessageModel(MessageModel.Role.System, instruction);
+            _user = new MessageModel(MessageModel.Role.User, input);
+
+            _httpClient = new HttpClient();
+
+
+            _httpClient.BaseAddress = new Uri("https://api.openai.com/");
+            _httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+        }
+
+        public async Task<string> FetchCompletionsAsync()
+        {
+            var requestData = new
+            {
+                model = "gpt-3.5-turbo",
+                messages = new[] { _system, _user },
+                temperature = 0.0,
+                max_tokens = 256
+            };
+
+            string json = System.Text.Json.JsonSerializer.Serialize(requestData);
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await _httpClient.PostAsync("v1/chat/completions", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                // Handle the error
+                Console.WriteLine($"Error: {response.StatusCode}");
+                return null;
+            }
+        }
+    }
+}
